a3fafb9a609aefe21daa7512e9c9739295fef81d
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-10 11:22:39 -0700

    update mi busqueda binaria

--- POO-Python/mi_busqueda_binaria_recur.py
+++ POO-Python/mi_busqueda_binaria_recur.py
@@ -1,4 +1,4 @@
-def binary_search_recursive(list_1, left, right, objective):
+def recursive_binary_search(list_1, left, right, objective):
 
     if left > right:
         return False
@@ -12,13 +12,13 @@ def binary_search_recursive(list_1, left, right, objective):
         return list_1[middle]
     
     if list_1[middle] < objective:
-        return binary_search_recursive(list_1, middle + 1, right, objective)
+        return recursive_binary_search(list_1, middle + 1, right, objective)
     else:
-        return binary_search_recursive(list_1, left, right - 1, objective)
+        return recursive_binary_search(list_1, left, right - 1, objective)
 
 if __name__ == '__main__':
 
-    list_1 = [1,2,3,4,5,6,7,8,9,10]
+    list_1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23]
 
-    result = binary_search_recursive(list_1, 0, len(list_1), 15)
+    result = recursive_binary_search(list_1, 0, len(list_1), 19)
     print(result)
