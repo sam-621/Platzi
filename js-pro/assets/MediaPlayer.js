d344b34ef5a829a6887b2ec19704eee634b872f2
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-02-28 12:28:17 -0700

    add plugins

--- js-pro/assets/MediaPlayer.js
+++ js-pro/assets/MediaPlayer.js
@@ -1,6 +1,16 @@
 function MediaPlayer(config) {
 	this.media = config.element;
+	this.plugins = config.plugins || [];
+
+	this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+	console.log(this);
+	this.plugins.forEach((plugin) => {
+		plugin.run(this);
+	});
+};
 
 MediaPlayer.prototype.play = function () {
 	this.media.play();
@@ -8,6 +18,14 @@ MediaPlayer.prototype.play = function () {
 
 MediaPlayer.prototype.pause = function () {
 	this.media.pause();
+};
+
+MediaPlayer.prototype.mute = function () {
+	this.media.muted = true;
+};
+
+MediaPlayer.prototype.unMute = function () {
+	this.media.unmute = false;
 };
 
 export default MediaPlayer;
--- js-pro/assets/index.js
+++ js-pro/assets/index.js
@@ -1,7 +1,8 @@
 import MediaPlayer from './MediaPlayer.js';
 const video = document.querySelector('video');
 const button = document.querySelector('button');
+import AutoPlay from './plugins/AutoPlay.js';
 
-const player = new MediaPlayer({ element: video });
+const player = new MediaPlayer({ element: video, plugins: [new AutoPlay()] });
 
 button.onclick = () => (player.media.paused ? player.play() : player.pause());
