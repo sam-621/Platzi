219671cd4860895cec673136eb0645ad636d6822
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-03-14 20:52:55 -0600

    add webpack config

---
+++ webpack-course/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+};
