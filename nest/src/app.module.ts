e201013b23fe135a7a348f0c076cd943f4da3b06
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-10-20 21:15:48 -0600

    learn nest

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
+const API_KEY = '12345634';
+const API_KEY_PROD = 'PROD1212121SA';
 
 @Module({
-  imports: [],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove properties that are not in the DTO but dont show any warning
+      forbidNonWhitelisted: true, // instead of stripping non-whitelisted properties validator will throw an exception.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
